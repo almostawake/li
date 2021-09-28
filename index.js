18372f66e18f9ca192dd8ac7d6172b435f154741
Andrew Walker <andrew@almostawake.com>
2021-09-28 04:29:51 +0000

    basic setup and scrape working as a template

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const { Storage } = require('@google-cloud/storage');
 const puppeteer = require('puppeteer');
+const takeScreenshots = false;
 
 
 exports.readProfiles = async (req, res) => {
     console.log('started readProfiles');
-    const page = await setup(req.body.cookies);
-    //todo loop through each profile in "profiles", scrape something
-    //todo clean up browser.close()
-    //todo return an array of scraped profiles
-    for (const profile of req.body.profiles) {
-        console.log(`reading ${profile}`)
-        await readProfile(page, profile);
+    const results = {};
+    results.cookieUserId = 'dunno yet'; // will hold id of the user whose cookies we're using
+    results.profiles = []; // will hold the profile scrapes
+    const { page, cookieUserId } = await setup(req.body.cookies);
+    results.cookieUserId = cookieUserId;
+    for (const profileUserId of req.body.profiles) {
+        results.profiles.push(await readProfile(page, profileUserId)); // add scraped profile info to results
     }
-    res.status(200).send('ok');
+    await page.browser().close();
+    console.log(results);
+    res.status(200).send(JSON.stringify(results));
 }
 
+// set up puppeteer using cookies saved with chome plugin "Export cookie JSON file for Puppeteer"
 setup = async (cookies) => {
 
-    // Initialise browser, page
+    console.log('setting up puppeteer with supplied cookies');
     const browser = await puppeteer.launch({ args: ['--lang=en-au,en', '--no-sandbox'] });
     const page = await browser.newPage();
     await page.setViewport({ width: 2048, height: 1300 });
@@ -25,21 +29,33 @@ setup = async (cookies) => {
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36');
     await page.setCookie(...cookies);
 
-    // Navigate to the login page
+    // Navigate to feeds page for the supplied user (from their cookies)
     await page.goto('https://www.linkedin.com/feed/', { waitUntil: 'networkidle2' });
 
     // Take a screenshot to Google Cloud Storage
     await takeScreenshot(page, '01-onload');
 
-    return page;
+    // Scrape the profile ID for the user whose cookies we have
+    const cookieUserURL = await page.$eval('.feed-identity-module a', a => a.getAttribute('href'));
+    const cookieUserId = cookieUserURL.split('/')[2];
+
+    return { page, cookieUserId };
 }
 
-readProfile = async (page, profile) => {
-    await page.goto(`https://www.linkedin.com/in/${profile}/`, { waitUntil: 'networkidle2' });
-    await takeScreenshot(page, `02-${profile}`);
+// read one profile page, return scraped text as an object
+readProfile = async (page, profileUserId) => {
+    console.log(`reading ${profileUserId}`)
+    await page.goto(`https://www.linkedin.com/in/${profileUserId}/`, { waitUntil: 'networkidle2' });
+    await takeScreenshot(page, `02-${profileUserId}`);
+    const profileUserName = await page.$eval('h1.text-heading-xlarge', h1 => h1.textContent);
+    const profileTagline = await page.$eval('.text-body-medium.break-words', div => div.innerText);
+    const profileConnectionDistance = await page.$eval('.distance-badge .dist-value', span => span.innerText);
+    return { profileUserId, profileUserName, profileTagline, profileConnectionDistance };
 }
 
+// if required, take screenshots to Google Cloud Storage bucket "almostawake-screenshots"
 takeScreenshot = async (page, filename) => {
+    if (!takeScreenshots) return;
     const storage = new Storage();
     const screenshotBinary = await page.screenshot({ encoding: 'binary' });
     const bucket = storage.bucket('almostawake-screenshots');
