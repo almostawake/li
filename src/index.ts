1509862d989d325e06b1756d4b026efdf7394bb8
Andrew Walker <andrew@almostawake.com>
2021-10-12 06:04:31 +0000

    convert to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-const { Storage } = require('@google-cloud/storage');
-const puppeteer = require('puppeteer');
+import { Request, Response } from "express";
+import { Storage } from '@google-cloud/storage';
+import puppeteer, { Page, Protocol } from 'puppeteer';
+
 const takeScreenshots = false;
+interface IResults { 
+    cookieUserId: string | undefined
+    profiles: any []
+ };
 
-
-exports.readProfiles = async (req, res) => {
+export const readProfiles = async (req: Request, res:Response) => {
     console.log('started readProfiles');
-    const results = {};
-    results.cookieUserId = 'dunno yet'; // will hold id of the user whose cookies we're using
-    results.profiles = []; // will hold the profile scrapes
+    const results:IResults = {
+        cookieUserId: "",
+        profiles: []
+    };
+
     const { page, cookieUserId } = await setup(req.body.cookies);
     results.cookieUserId = cookieUserId;
     for (const profileUserId of req.body.profiles) {
@@ -19,7 +26,7 @@ exports.readProfiles = async (req, res) => {
 }
 
 // set up puppeteer using cookies saved with chome plugin "Export cookie JSON file for Puppeteer"
-setup = async (cookies) => {
+const setup = async (cookies: Protocol.Network.CookieParam[]) => {
 
     console.log('setting up puppeteer with supplied cookies');
     const browser = await puppeteer.launch({ args: ['--lang=en-au,en', '--no-sandbox'] });
@@ -37,34 +44,36 @@ setup = async (cookies) => {
 
     // Scrape the profile ID for the user whose cookies we have
     const cookieUserURL = await page.$eval('.feed-identity-module a', a => a.getAttribute('href'));
-    const cookieUserId = cookieUserURL.split('/')[2];
+    const cookieUserId = cookieUserURL?.split('/')[2];
 
     return { page, cookieUserId };
 }
 
 // read one profile page, return scraped text as an object
-readProfile = async (page, profileUserId) => {
+const readProfile = async (page: Page, profileUserId: string) => {
     console.log(`reading ${profileUserId}`)
     await page.goto(`https://www.linkedin.com/in/${profileUserId}/`, { waitUntil: 'networkidle2' });
     await takeScreenshot(page, `02-${profileUserId}`);
     const profileUserName = await page.$eval('h1.text-heading-xlarge', h1 => h1.textContent);
-    const profileTagline = await page.$eval('.text-body-medium.break-words', div => div.innerText);
-    const profileConnectionDistance = await page.$eval('.distance-badge .dist-value', span => span.innerText);
-    const profileArea = await page.$eval('.text-body-small.inline.t-black--light.break-words', span => span.innerText);
-    const connectionLink = await page.$eval('ul.pv-top-card--list.pv-top-card--list-bullet.display-flex.pb1>li.text-body-small > a.ember-view', a => a.href);
+    const profileTagline = await page.$eval('.text-body-medium.break-words', div => div.textContent);
+    const profileConnectionDistance = await page.$eval('.distance-badge .dist-value', span => span.textContent);
+    const profileArea = await page.$eval('.text-body-small.inline.t-black--light.break-words', span => (span as HTMLElement).innerText);
+    const connectionLink = await page.$eval('ul.pv-top-card--list.pv-top-card--list-bullet.display-flex.pb1>li.text-body-small > a.ember-view', a => (a as HTMLAnchorElement).href);
     await page.goto(`https://www.linkedin.com/in/${profileUserId}/detail/contact-info`, { waitUntil: 'networkidle2' });
-    const contactLinks = await page.$$eval('.pv-contact-info__contact-link', anchors => anchors.map(anchor => [anchor.href]));
+    const contactLinks = await page.$$eval('.pv-contact-info__contact-link', anchors => anchors.map(anchor => [(anchor as HTMLAnchorElement).href]));
     await page.goto(connectionLink, { waitUntil: 'networkidle2' });
-    const connectionLinks = await page.$$eval('.entity-result__title-text>.app-aware-link', links => links.map(link => [link.innerText.split("\n")[0], link.href]));
+    const connectionLinks = await page.$$eval('.entity-result__title-text>.app-aware-link', links => links.map(link => [(link as HTMLAnchorElement).innerText.split("\n")[0], (link as HTMLAnchorElement).href]));
     return { profileUserId, profileUserName, profileTagline, profileConnectionDistance, profileArea, contactLinks, connectionLinks};
 }
 
 // if required, take screenshots to Google Cloud Storage bucket "almostawake-screenshots"
-takeScreenshot = async (page, filename) => {
+const takeScreenshot = async (page:Page, filename:string) => {
     if (!takeScreenshots) return;
     const storage = new Storage();
     const screenshotBinary = await page.screenshot({ encoding: 'binary' });
     const bucket = storage.bucket('almostawake-screenshots');
     const file = bucket.file(`${filename}.png`);
-    await file.save(screenshotBinary, { metadata: { contentType: 'image/png' }, });
+    if (screenshotBinary) {
+        await file.save(screenshotBinary, { metadata: { contentType: 'image/png' }, });
+    }
 }
